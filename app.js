abaf21cf1427835834f06101dc0d4c84e927bca8
Simranjit Singh <simranjit.boyal@gmail.com>
2018-05-19 14:30:15 -0600

    Added images

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ app.get('/', (req,res)=>{
 });
 
 app.get('/campgrounds', (req,res)=>{
-    
+    var campgrounds = [
+        {name:"Salmon Creek", image:"https://farm8.staticflickr.com/7677/17482091193_e0c121a102.jpg"},
+        {name:"Granite Hill", image:"https://farm6.staticflickr.com/5452/16812275854_221a979b72.jpg"},
+        {name:"Mountain Goat's Rest", image:"https://farm3.staticflickr.com/2947/15215548990_efc53d32b6.jpg"}
+    ]
+    res.render("campgrounds",{campgrounds : campgrounds});
 });
 
 
